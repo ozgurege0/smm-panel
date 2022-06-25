58123450048e0196d11187eefc7cf8699a1f3bf2
ozgurege0 <81050186+ozgurege0@users.noreply.github.com>
2022-06-25 13:28:51 +0300

    Update logout.php

--- admin-panel/logout.php
+++ admin-panel/logout.php
@@ -81,7 +81,7 @@
 			</div><!--end row-->
 		</div><!-- container -->
 		<footer class="footer text-center text-sm-left">
-			&copy; 2021 <?=$ayar['baslik']?> <span class="text-muted d-none d-sm-inline-block float-right">Ravertan <i class="mdi mdi-heart text-danger"></i> ile kodlanmıştır.</span>
+			&copy; 2022 <?=$ayar['baslik']?> <span class="text-muted d-none d-sm-inline-block float-right">Ozgur_Medya <i class="mdi mdi-heart text-danger"></i> ile kodlanmıştır.</span>
 		</footer>
 	</div>
 	<!-- end page content -->
