62c05a1a24b8b97f195fbe2dc7f6cadaa0f5ecb5
ozgurege0 <81050186+ozgurege0@users.noreply.github.com>
2022-06-25 12:28:12 +0300

    Add files via upload

---
+++ assets/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'vendor_name/htdocs',
+  ),
+  'versions' => 
+  array (
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.1.6',
+      'version' => '6.1.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c2796cb1cb99d7717290b48c4e6f32cb6c60b7b3',
+    ),
+    'roave/security-advisories' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6d2e5ab854782830911ddd33b7d4649b9f18c10f',
+    ),
+    'vendor_name/htdocs' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
